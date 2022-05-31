1ec98f7b4da68a1cec8be8fc86d135e4682f3db9
StellahMbao <woodzdj@gmail.com>
2022-05-31 12:03:47 +0300

    modified readme.md and .py file

---
+++ python.py
@@ -0,0 +1,18 @@
+print ("   /|")
+print ("  / | ")
+print (" /  |")
+print ("/___|")
+
+print('Come \nover')
+
+print ('Mike\'s brother')
+
+# concantenation
+first_sentence = "Go to the shop and get me a few things,"
+second_sentence = "Don't forget to pass by the grocery store to pick up my prescription"
+print(first_sentence + ' ' + second_sentence)
+print(first_sentence.upper())
+
+print(second_sentence.isupper())
+print(second_sentence.upper().isupper())
+print(len(first_sentence))
